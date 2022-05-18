ac0f14ae244946914c9939f02b3b5aae2992e488
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-18 11:20:50 -0300

    mainheader starting style and html added

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar p-2">
+    <div class="container-fluid d-flex">
+      <a class="navbar-brand" href="/"><img src="../assets/Logo.svg" alt="Brand Logo" /></a>
+      <button class="btn btn-outline-light" type="submit">Favorites</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-color: #000032;
+}
+</style>
