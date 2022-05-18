676627ad0d43e6ef1cf4dfc2731f89ddf8455e13
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-18 11:19:18 -0300

    main header import and logo added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <main-header />
   <router-view />
 </template>
 
 <script>
 import "./styles/global.css";
-
+import MainHeader from "./components/MainHeader.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    MainHeader,
+  },
 };
 </script>
