cc2817d807aa2132beed85cca907687b1c0606ec
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-19 09:38:19 -0300

    style: coin stats table

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,69 +1,86 @@
 <template>
-  <div>
-    <div>
-      <img :src="coin.image" style="width: 2rem" class="me-2" />
-      <span class="text-uppercase">{{ coin.name }} ({{ coin.symbol }})</span>
+  <div v-for="coin in coins" :key="coin.id" class="container p-2">
+    <!-- Coin Price -->
+    <div class="coin-price d-flex justify-content-between p-3">
+      <div class="d-flex align-items-center">
+        <img :src="coin.image" style="width: 3rem" class="me-2" />
+        <h1 class="me-2">{{ coin.name }}</h1>
+        <h3 class="text-uppercase">({{ coin.symbol }})</h3>
+      </div>
+      <div class="d-flex align-items-center">
+        <h2 class="me-2">${{ coin.current_price.toFixed(2) }}</h2>
+        <h5 :class="[coin.price_change_percentage_24h > 0 ? success : danger]">
+          {{ coin.price_change_percentage_24h.toFixed(2) }}%
+        </h5>
+      </div>
     </div>
-    <div>
-      <span>{{ coin.current_price }}</span>
-      <span>{{ coin.price_change_percentage_24h }}</span>
-    </div>
-    <div>
-      <div>
-        <div class="progress">
-          <div
-            class="progress-bar progress-bar-striped progress-bar-animated"
-            role="progressbar"
-            :aria-valuenow="`${(coin.current_price / coin.high_24h) * 100}`"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            :style="{ width: `${(coin.current_price / coin.high_24h) * 100}%` }"
-          ></div>
+    <!-- Coin Price End -->
+    <!-- Coin Stats -->
+    <div class="coin-stats container p-3">
+      <div class="row">
+        <div class="col d-flex flex-column justify-content-between align-items-center p-3">
+          <span>Market Cap</span>
+          <span>${{ coin.market_cap.toLocaleString() }}</span>
         </div>
-        <div>
-          <span>{{ coin.low_24h }}</span>
-          <span>24h Range</span>
-          <span>{{ coin.high_24h }}</span>
+        <div class="col d-flex flex-column justify-content-between align-items-center p-3">
+          <span class="p-1">24 Hour Trading Vol</span>
+          <span class="p-1">${{ coin.total_volume.toLocaleString() }}</span>
+        </div>
+        <div class="col d-flex flex-column justify-content-between align-items-center p-3">
+          <span class="p-1">Fully Diluted Valuation</span>
+          <span class="p-1"
+            >${{
+              coin.fully_diluted_valuation == null
+                ? "-"
+                : coin.fully_diluted_valuation.toLocaleString()
+            }}</span
+          >
         </div>
       </div>
-      <div>
-        <div>
-          <div>
-            <span>Market Cap</span>
-            <span>{{ coin.market_cap }}</span>
-          </div>
-          <div>
-            <span>24 Hour Trading Vol</span>
-            <span>{{ coin.total_volume }}</span>
-          </div>
-          <div>
-            <span>Fully Diluted Valuation</span>
-            <span>{{
-              coin.fully_diluted_valuation == null ? "-" : coin.fully_diluted_valuation
-            }}</span>
-          </div>
+      <div class="row">
+        <div class="col d-flex flex-column justify-content-between align-items-center p-3">
+          <span class="p-1">Circulating Supply</span>
+          <span class="p-1">{{ coin.circulating_supply.toLocaleString() }}</span>
         </div>
-        <div>
-          <div>
-            <span>Circulating Supply</span>
-            <span>{{ coin.circulating_supply }}</span>
-          </div>
-          <div>
-            <span>Total Supply</span>
-            <span>{{ coin.total_supply == null ? "-" : coin.total_supply }}</span>
-          </div>
-          <div>
-            <span>Max Supply</span>
-            <span>{{ coin.max_supply == null ? "-" : coin.max_supply }}</span>
-          </div>
+        <div class="col d-flex flex-column justify-content-between align-items-center p-3">
+          <span class="p-1">Total Supply</span>
+          <span class="p-1">{{
+            coin.total_supply == null ? "-" : coin.total_supply.toLocaleString()
+          }}</span>
+        </div>
+        <div class="col d-flex flex-column justify-content-between align-items-center p-3">
+          <span class="p-1">Max Supply</span>
+          <span class="p-1">{{
+            coin.max_supply == null ? "-" : coin.max_supply.toLocaleString()
+          }}</span>
         </div>
       </div>
     </div>
-    <div>
-      <label for="data-historica">Date (history)</label>
-      <input type="date" name="data-historica" @change="getHistoricalValue" />
-      <span>{{ historicalValue }}</span>
+    <!-- Coin Stats End -->
+    <div class="m-3">
+      <label for="from">Price history from:</label>
+      <input type="date" name="from" @change="getHistoricalValue" />
+      <label for="to">to:</label>
+      <input type="date" name="to" @change="getHistoricalValue" />
+      <!-- <span>{{ historicalValue }}</span> -->
     </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col" v-for="title in titles" :key="title">{{ title }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr scope="row" v-for="(values, index) in marketValue.prices" :key="index">
+          <td>
+            {{ new Date(marketValue.prices[index][0]).toLocaleString().split(" ")[0] }}
+          </td>
+          <td>${{ marketValue.prices[index][1].toFixed(2).toLocaleString() }}</td>
+          <td>${{ marketValue.market_caps[index][1].toFixed(0).toLocaleString() }}</td>
+          <td>${{ marketValue.total_volumes[index][1].toLocaleString() }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -73,28 +90,49 @@ export default {
   name: "CoinDetail",
   data() {
     return {
-      coin: [],
+      coins: [],
       historicalValue: 0,
+      success: "text-success",
+      danger: "text-danger",
+      marketValue: {},
+      titles: ["Date", "Price", "Market Cap", "24 Hour Trading Vol"],
+      from: 0,
+      to: Number(new Date()),
     };
   },
   mounted() {
-    this.getCoin();
+    this.getCoin(), this.getMarketChart();
   },
   methods: {
     async getCoin() {
       const response = await api.get(
         `/coins/markets?vs_currency=usd&ids=${this.$route.params.coin}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
       );
-      //console.log(response.data);
-      this.coin = response.data[0];
+      this.coins = response.data;
+      //console.log(this.coins);
     },
-    async getHistoricalValue(e) {
-      let date = e.target.value.split("-").reverse().join("-");
+    // async getHistoricalValue(e) {
+    //   let date = e.target.value.split("-").reverse().join("-");
+    //   const response = await api.get(
+    //     `/coins/${this.$route.params.coin}/history?date=${date}&localization=false`
+    //   );
+    //   // console.log(response.data.market_data.current_price.usd);
+    //   this.historicalValue = response.data.market_data.current_price.usd;
+    // },
+    async getMarketChart() {
+      //let date = e.target.value.split("-").reverse().join("-");
+      if (!this.from) {
+        let date = new Date();
+        date.setDate(date.getHours() - 4);
+        this.from = this.to - 86400 * 3;
+        console.log("aqui", this.from);
+      }
       const response = await api.get(
-        `/coins/${this.$route.params.coin}/history?date=${date}&localization=false`
+        `/coins/${this.$route.params.coin}/market_chart/range?vs_currency=usd&from=${this.from}&to=${this.to}`
       );
-      // console.log(response.data.market_data.current_price.usd);
-      this.historicalValue = response.data.market_data.current_price.usd;
+      this.marketValue = response.data;
+      console.log("aqui 2", this.from);
+      console.log("aqui 3", this.to);
     },
     updateCoin() {
       setTimeout(() => {
@@ -103,11 +141,19 @@ export default {
     },
   },
   watch: {
-    coin() {
+    coins() {
       this.updateCoin();
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  color: #000032;
+}
+.coin-stats {
+  border: 2px solid #000032;
+  border-radius: 30px;
+}
+</style>
