87f6fdf73c8c20760c9d692f0c324f291ecea77c
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-05-19 23:06:12 -0300

    fix: link header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar p-2">
     <div class="container-sm d-flex justify-content-center justify-content-sm-between">
-      <a class="navbar-brand" href="/"><img src="../assets/Logo.svg" alt="Brand Logo" /></a>
+      <a class="navbar-brand" :href="homeRef"><img src="../assets/Logo.svg" alt="Brand Logo" /></a>
     </div>
   </nav>
 </template>
@@ -9,6 +9,11 @@
 <script>
 export default {
   name: "MainHeader",
+  data() {
+    return {
+      homeRef: process.env.NODE_ENV === "production" ? "/crypto-vuejs-dacxi/" : "/",
+    };
+  },
 };
 </script>
 
